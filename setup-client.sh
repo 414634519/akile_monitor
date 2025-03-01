d54f166c886e7e79297a1c4d4e307585760ecb2a
Bruce <139223400+414634519@users.noreply.github.com>
2025-03-01 17:46:58 +0800

    Update setup-client.sh

--- setup-client.sh
+++ setup-client.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
+SECRET=$1
+URL=$2
+NAME=$3
 
-if [ "$#" -ne 3 ]; then
- echo "Usage: $0 <auth_secret> <url> <name>"
- exit 1
+if [ -z "$SECRET" ] || [ -z "$URL" ] || [ -z "$NAME" ]; then
+    echo "Usage: $0 <secret> <url> <name>"
+    exit 1
 fi
-
-auth_secret="$1"
-url="$2"
-monitor_name="$3"
 
 ARCH=$(uname -m)
+
 if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; then
-  CLIENT_FILE="akile_client-linux-amd64"
+    CLIENT_FILE="akile_client-linux-amd64"
 elif [[ "$ARCH" == "i686" ]] || [[ "$ARCH" == "x86" ]]; then
-  CLIENT_FILE="akile_client-linux-386"
+    CLIENT_FILE="akile_client-linux-386"
 elif [[ "$ARCH" == "aarch64" ]] || [[ "$ARCH" == "arm64" ]]; then
-  CLIENT_FILE="akile_client-linux-arm64"
+    CLIENT_FILE="akile_client-linux-arm64"
 else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
+    echo "Unsupported architecture: $ARCH"
+    exit 1
 fi
 
-
-mkdir -p ~/ak_monitor/
-cd ~/ak_monitor/
-
-wget -O client "https://github.com/akile-network/akile_monitor/releases/latest/download/$CLIENT_FILE" --no-check-certificate
+wget -O client "https://github.com/akile-network/akile_monitor/releases/latest/download/$CLIENT_FILE"
 chmod +x client
 
-cat > client.json << EOF
-{
-  "auth_secret": "${auth_secret}",
-  "url": "${url}",
-  "name": "${monitor_name}"
-}
-EOF
+nohup ./client -s $SECRET -u $URL -n $NAME >/dev/null 2>&1 &
 
-nohup ./client > client.log 2>&1 &
-
-if pgrep -f client > /dev/null; then
-  echo "Akile Monitor Client is running successfully!"
-else
-  echo "Failed to start Akile Monitor Client."
-fi
+echo "Akile Monitor Client is running successfully!"
